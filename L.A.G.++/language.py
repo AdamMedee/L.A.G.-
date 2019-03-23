1616e4581b15eec3e3c4ee2ffc7a28f1e6936087
AdamMehdi <adam_mehdi@hotmail.ca>
2019-03-23 12:19:09 -0400

    stuff

--- L.A.G.++/language.py
+++ L.A.G.++/language.py
@@ -7,12 +7,14 @@ class Code:
         self.curline = 0
         self.vars = {}
         self.const = {}
+        self.operations = ["+", "-", "*", "/", "%", "//", "^"]
 
     def runLine(self):
         pass
 
     def bracketChase(self, pos, t1, t2):
         count = 1
+        orig = pos
         while True:
             if (pos[1]+1) == len(self.code[pos[0]]):
                 pos[1] = 0
@@ -22,12 +24,35 @@ class Code:
                 count -= 1
             elif self.code[pos[0]][pos[1]] == "t1":
                 count += 1
-        if count == 0:
-            return pos
+            if count == 0:
+                return pos
+            elif pos[0]+1 == len(self.code) and pos[1]+1 == len(self.code[pos[0]]):
+                self.errorMessage("Missing End Bracket", orig)
+                break
+
+    def defineVar(self, line):
+        a, b = line.split("=")
+        self.vars[a] = self.evaluate(b)
+
+    def changeVar(self, line, op):
+        a, b = line.split(op+"=")
+        if a in self.vars.keys():
+            self.vars[a] = self.evaluate(b)
+        else:
+            self.errorMessage("Variable Not Defined", a)
+
+    def readIf(self, lineNum):
+        s = self.code[lineNum].find("(")
 
 
-    def errorMessage(self, error):
+    def evaluate(self, code):
+        pass
+
+    def errorMessage(self, error, info):
         pass
 
     def output(self, msg):
         print(msg)
+
+    def l_input(self):
+        pass
