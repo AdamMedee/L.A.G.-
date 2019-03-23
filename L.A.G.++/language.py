e0f20a3c415c0748c35bad7381a8744c7fd27c61
AdamMehdi <adam_mehdi@hotmail.ca>
2019-03-23 12:04:03 -0400

    bracket chasing

--- L.A.G.++/language.py
+++ L.A.G.++/language.py
@@ -5,14 +5,29 @@ class Code:
     def __init__(self, code):
         self.code = code
         self.curline = 0
+        self.vars = {}
+        self.const = {}
 
     def runLine(self):
         pass
 
-    def bracketChase(self, pos):
+    def bracketChase(self, pos, t1, t2):
         count = 1
         while True:
-            pass
+            if (pos[1]+1) == len(self.code[pos[0]]):
+                pos[1] = 0
+            else:
+                pos[1] += 1
+            if self.code[pos[0]][pos[1]] == "t2":
+                count -= 1
+            elif self.code[pos[0]][pos[1]] == "t1":
+                count += 1
+        if count == 0:
+            return pos
+
 
     def errorMessage(self, error):
         pass
+
+    def output(self, msg):
+        print(msg)
