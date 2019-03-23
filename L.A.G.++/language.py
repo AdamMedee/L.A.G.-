85132cc8925a3f72bc4dadad5a2fc78cee48d50b
AdamMehdi <adam_mehdi@hotmail.ca>
2019-03-23 11:47:12 -0400

    code stuff for lang

---
+++ L.A.G.++/language.py
@@ -0,0 +1,18 @@
+from pygame import *
+from math import *
+
+class Code:
+    def __init__(self, code):
+        self.code = code
+        self.curline = 0
+
+    def runLine(self):
+        pass
+
+    def bracketChase(self, pos):
+        count = 1
+        while True:
+            pass
+
+    def errorMessage(self, error):
+        pass
